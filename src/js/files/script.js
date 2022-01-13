0a0e625495d52c08add20d597f7efa861f418ef7
Idiot <verruckterdrachen@gmail.com>
2022-01-13 15:59:58 +0300

    add footer

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -3,4 +3,25 @@ import { isMobile } from "./functions.js";
 // Подключение списка активных модулей
 import { flsModules } from "./modules.js";
 
+let center = [47.24483840526133, 39.72360365302512];
+let zoom = 17;
+
+function init() {
+	let map = new ymaps.Map('map', {
+		center: center,
+		zoom: zoom,
+	});
+	map.controls.remove('geolocationControl'); // удаляем геолокацию
+	map.controls.remove('searchControl'); // удаляем поиск
+	map.controls.remove('trafficControl'); // удаляем контроль трафика
+	map.controls.remove('typeSelector'); // удаляем тип
+	map.controls.remove('fullscreenControl'); // удаляем кнопку перехода в полноэкранный режим
+	map.controls.remove('zoomControl'); // удаляем контрол зуммирования
+	map.controls.remove('rulerControl'); // удаляем контрол правил
+	//map.behaviors.disable(['scrollZoom']); // отключаем скролл карты (опционально)
+}
+
+ymaps.ready(init);
+
+
 
